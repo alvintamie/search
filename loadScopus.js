d1b2a1aed3fceddb4a3cc7abe0f8c191595c1a5b
alvi0010 <alvintamie@gmail.com>
2012-09-06 08:51:01 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -25,21 +25,22 @@ function getContextCallback(response) {
 
 function getRef(response){
 	console.log("hello");
-	console.log("1: "+ response.data);	    
+//	console.log("1: "+ response.data);	    
 	var temp = JSON.parse(response.data);
 	console.log(temp);
 	console.log(temp['abstracts-retrieval-response']['references']['reference'].length);
 	for(var i=0;i<temp['abstracts-retrieval-response']['references']['reference'].length;i++){
 		scopusId=temp['abstracts-retrieval-response']['references']['reference'][i]['scopus-id'];
 var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+scopusId+"?view=FULL");
+	console.log(i+" : ");
 gadgets.sciverse.makeContentApiRequest(urlRef, getRefAbstract, requestHeaders);
 	}
 }
 
 function getRefAbstract(response){
-//	console.log("refAbstract v");
-//	var temp = JSON.parse(response.data);
-//	console.log(temp);
+	console.log("refAbstract v");
+	var temp = JSON.parse(response.data);
+	console.log(temp);
 }
 
 function panggil(){
