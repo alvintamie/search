1a09396d376d3f21306e117553d907e44474bd6c
alvi0010 <alvintamie@gmail.com>
2012-09-07 11:09:43 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -3,6 +3,7 @@
  var it;
  var referenceObject = new Array();
  var referenceSize;
+ var citedbySize;
  var citedbyUrl = new Array();
  var citedbyObject = new Array();
  
@@ -40,7 +41,25 @@ function getCitedby(response){
 	for(var i=1;i<temp['search-results']['link'].length;i++){
 		citedbyUrl.push(temp['search-results']['link'][i]['@href']);
 		console.log(temp['search-results']['link'][i]['@href']);
-	}}
+	}
+	citedbySize= temp['search-results']['entry'].length;
+	for(var i=0;i<temp['search-results']['entry'].length;i++){
+		var Obj= new Object();
+		Obj.abstract = temp['search-results']['entry'][i]['dc:description'];
+		Obj.title =    temp['search-results']['entry'][i]['dc:title'];
+       		Obj.type =     temp['search-results']['entry'][i]['subtypeDescription'];
+       		Obj.citedbyCount = temp['search-results']['entry'][i]['citedby-count'];
+       		Obj.creator= temp['search-results']['entry'][i]['creator'];
+       		Obj.publicationName = temp['search-results']['entry'][i]['prism:publicationName'];
+       		var tempId=temp['search-results']['entry'][i]['dc:identifier'].split(":");
+       		Obj.identifier= tempId[1];
+     	  	Obj.date = temp['search-results']['entry'][i]['prism:coverDate'];
+       		Obj.volume = temp['search-results']['entry'][i]['prim:volume'];
+       		Obj.author=temp['search-results']['entry'][i]['author'];     	
+       		Obj.affiliation= temp['search-results']['entry'][i]['affiliation'];
+		citedbyObject.push(Obj);
+		}
+	}
 }
 function getRef(response){
 	console.log("hello");
@@ -75,7 +94,7 @@ console.log(it++);
       		Obj.title = temp['abstracts-retrieval-response']['coredata']['dc:title'];
        		Obj.type = temp['abstracts-retrieval-response']['coredata']['prism:aggregation Type'];
        		Obj.citedbyCount = temp['abstracts-retrieval-response']['coredata']['citedby-count'];
-       		Obj.publicationName = temp['abstracts-retrieval-response']['coredata']['dc:description'];
+       		Obj.publicationName = temp['abstracts-retrieval-response']['coredata']['prism:publicationName'];
        		var tempId=temp['abstracts-retrieval-response']['coredata']['dc:identifier'].split(":");
        		Obj.identifier= tempId[1];
      	  	Obj.date = temp['abstracts-retrieval-response']['coredata']['prism:coverDate'];
