d2d87c0371934960a7d5a3b55ec2e2de92e7b974
alvi0010 <alvintamie@gmail.com>
2012-09-07 12:34:38 -0700

    Update loadCitedby.js

--- loadCitedby.js
+++ loadCitedby.js
@@ -3,7 +3,11 @@ function loadCitedby(index){
   requestHeaders['X-ELS-APIKey'] = "0efd37da9830a0e7f43dbe261f5f7956";
   requestHeaders['X-ELS-Authtoken'] = context.secureAuthtoken;      
   requestHeaders['Accept'] = "application/json, text/xml";
-  gadgets.sciverse.makeContentApiRequest(citedbyUrl[index], loadCitedbyCallback, requestHeaders);
+  
+  var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?start=25&count=10&query=refeid
+  (2-s2.0-"+context.scDocId+")&view=COMPLETE");
+
+  gadgets.sciverse.makeContentApiRequest(urlCitedby, loadCitedbyCallback, requestHeaders);
   
 }
 
