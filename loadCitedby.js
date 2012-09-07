12e233a5b7ff69db5950574985ed6c7ab11ac6dd
alvi0010 <alvintamie@gmail.com>
2012-09-07 12:54:34 -0700

    Update loadCitedby.js

--- loadCitedby.js
+++ loadCitedby.js
@@ -3,8 +3,11 @@ function loadCitedby(index){
   requestHeaders['X-ELS-APIKey'] = "0efd37da9830a0e7f43dbe261f5f7956";
   requestHeaders['X-ELS-Authtoken'] = context.secureAuthtoken;      
   requestHeaders['Accept'] = "application/json, text/xml";
+  if((index+1)*25<totalCitation)
+  var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?start="+index*25+"&count=25&query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE");
+  else 
+  var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?start="+index*25+"&count="+totalCitation-index*25+"25&query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE");
   
-  var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?start=0&count=10&query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE");
   console.log(context.scDocId);
   gadgets.sciverse.makeContentApiRequest(urlCitedby, loadCitedbyCallback, requestHeaders);
   
