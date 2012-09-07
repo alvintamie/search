6fa60e44080f0602d244da0fd29b44ca06916687
alvi0010 <alvintamie@gmail.com>
2012-09-07 11:40:52 -0700

    Update loadCitedby.js

--- loadCitedby.js
+++ loadCitedby.js
@@ -1,10 +1,9 @@
-function loadCitedby(index){
-  citedbyUrl    
+function loadCitedby(index){  
   requestHeaders['Accept-Charset'] = "utf-8";
   requestHeaders['X-ELS-APIKey'] = "0efd37da9830a0e7f43dbe261f5f7956";
   requestHeaders['X-ELS-Authtoken'] = context.secureAuthtoken;      
   requestHeaders['Accept'] = "application/json, text/xml";
-  gadgets.sciverse.makeContentApiRequest(citedbyUrl, loadCitedbyCallback, requestHeaders);
+  gadgets.sciverse.makeContentApiRequest(citedbyUrl[index], loadCitedbyCallback, requestHeaders);
   
 }
 
