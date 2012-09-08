fb990a5d31611f48f19283024af94ecc02c3ca00
alvi0010 <alvintamie@gmail.com>
2012-09-07 21:37:28 -0700

    Update loadCitedby.js

--- loadCitedby.js
+++ loadCitedby.js
@@ -20,5 +20,20 @@ function loadCitedbyCallback(response){
   console.log("load citedby");      
 	var temp = JSON.parse(response.text);
 	console.log(temp);
-
+	for(var i=0;i<temp['search-results']['entry'].length;i++){
+		var Obj= new Object();
+		Obj.abstract = temp['search-results']['entry'][i]['dc:description'];
+		Obj.title =    temp['search-results']['entry'][i]['dc:title'];
+       		Obj.type =     temp['search-results']['entry'][i]['subtypeDescription'];
+       		Obj.citedbyCount = temp['search-results']['entry'][i]['citedby-count'];
+       		Obj.creator= temp['search-results']['entry'][i]['creator'];
+       		Obj.publicationName = temp['search-results']['entry'][i]['prism:publicationName'];
+       		var tempId=temp['search-results']['entry'][i]['dc:identifier'].split(":");
+       		Obj.identifier= tempId[1];
+     	  	Obj.date = temp['search-results']['entry'][i]['prism:coverDate'];
+       		Obj.volume = temp['search-results']['entry'][i]['prim:volume'];
+       		Obj.author=temp['search-results']['entry'][i]['author'];     	
+       		Obj.affiliation= temp['search-results']['entry'][i]['affiliation'];
+		citedbyObject.push(Obj);
+		}
 }
