5708125049cef534511a01cf4c496882765c02ff
alvi0010 <alvintamie@gmail.com>
2012-09-08 06:59:12 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -50,8 +50,8 @@ function getCoauthor(response){
 	console.log(temp);
 	
 		for(var i=0;i<temp['search-results']['entry'].length;i++){
-       		var tempId=temp['search-results']['entry'][i]['dc:identifier'].split(":");  	
-		var urlCoauthor=encodeURI( "http://api.elsevier.com/content/author/author_id:"+tempId);
+       		var urlCoauthor=temp['search-results']['entry'][i]['prism:url']; 	
+	                          
 		console.log(urlCoauthor);
 			gadgets.sciverse.makeContentApiRequest(urlCoauthor, getCoauthorCallback, requestHeaders);
 		}
