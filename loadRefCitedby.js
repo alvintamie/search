1f020e3852d5d4b1a59db003a3b40c1a8437d354
alvi0010 <alvintamie@gmail.com>
2012-09-08 00:08:38 -0700

    Update loadRefCitedby.js

--- loadRefCitedby.js
+++ loadRefCitedby.js
@@ -6,8 +6,8 @@ function getRefCitedby(scopusId,citedbyCount){
 }
 
 function loadRefCitedbyCallback(response){
-  
-    console.log("citedby ref " + ++countingRef);
+  ++countingRef;
+    console.log("citedby ref " + countingRef );
   var temp = JSON.parse(response.text);
 	console.log(temp);
 	try{ if(temp['service-error']['status']['statusCode']=='INVALID_INPUT'){
