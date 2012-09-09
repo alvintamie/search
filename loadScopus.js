dc8cfc890725a6e1112fad9eb65f593b63ca2f9c
alvi0010 <alvintamie@gmail.com>
2012-09-09 05:30:44 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -32,37 +32,6 @@ function getContextCallback(response) {
  	gadgets.sciverse.makeContentApiRequest(urlRef, getRef, requestHeaders);
  
 }
-function getCoauthor(response){
-    	console.log("coauthor");
-	var temp = JSON.parse(response.text);
-	console.log(temp);
-	if(!statusCoauthors){
-	totalCoauthors=temp['search-results']['opensearch:totalResults'];statusCoauthors=true;}
-	console.log("totalCoauthors : "+totalCoauthors);
-	for(var i=0;i<temp['search-results']['entry'].length;i++){
-       		var urlCoauthor=temp['search-results']['entry'][i]['prism:url']; 		                       
-		gadgets.sciverse.makeContentApiRequest(urlCoauthor, getCoauthorCallback, requestHeaders);
-		}
-}
-
-function getCoauthorCallback(response){
-//	console.log("coauthor details");
-	var temp = JSON.parse(response.text);
-//	console.log(temp);
-	var Obj= new Object();
-	coauthorObject.push(temp);
-}
-
-function loadCoauthor(index){  
-  if((index+1)*25<totalCoauthor){
-  var urlCoauthor=encodeURI( "http://api.elsevier.com/content/search/index:author?start="+index*25+"&count="+25+"&query=affil(university)&co-author="+context.au1Id); 
-  }
-  else 
-  {
-  var urlCoauthor=encodeURI( "http://api.elsevier.com/content/search/index:author?start="+index*25+"&count="+(totalCoauthor-index*25)+"&query=affil(university)&co-author="+context.au1Id); 
-  }
-  gadgets.sciverse.makeContentApiRequest(urlCoauthor, getCoauthor, requestHeaders);
-}
 
 
 
