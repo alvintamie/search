2109fe383ee3d5e3e8437322fac26fb9e955e8fc
alvi0010 <alvintamie@gmail.com>
2012-09-09 05:29:16 -0700

    Update loadReference.js

---
+++ loadReference.js
@@ -0,0 +1,51 @@
+function getRef(response){
+  console.log("get all ref");
+//	console.log("1: "+ response.data);	    
+	var temp = JSON.parse(response.data);
+	console.log(temp);
+	referenceSize=temp['abstracts-retrieval-response']['references']['reference'].length
+	console.log("SizeOfRef : "+temp['abstracts-retrieval-response']['references']['reference'].length);
+	currentReferenceSize=0;
+	for(var i=0;i<temp['abstracts-retrieval-response']['references']['reference'].length;i++){
+		scopusId=temp['abstracts-retrieval-response']['references']['reference'][i]['scopus-id'];
+	//	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+scopusId+"?view=FULL");
+		var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+scopusId+"?view=REF&startref=0");
+		gadgets.sciverse.makeContentApiRequest(urlRef, getRefAbstract, requestHeaders);
+	//	getRefCitedby(scopusId,temp['abstracts-retrieval-response']['references']['reference'][i]['citedby-count'])
+	}
+}
+
+function getRefAbstract(response){
+
+console.log(currentReferenceSize++ + "ref abstract");
+	var Obj= new Object();
+	if(!response.data) {
+		console.log("NULL reference");
+		Obj.available=false;
+		referenceObject.push(Obj);
+		return;}
+    	try{
+       		var temp = JSON.parse(response.text);
+       		console.log(temp);
+       		Obj.available=true;
+       		
+       		Obj.abstract = temp['abstracts-retrieval-response']['coredata']['dc:description'];
+      		Obj.title = temp['abstracts-retrieval-response']['coredata']['dc:title'];
+       		Obj.type = temp['abstracts-retrieval-response']['coredata']['prism:aggregation Type'];
+       		Obj.citedbyCount = temp['abstracts-retrieval-response']['coredata']['citedby-count'];
+       		Obj.publicationName = temp['abstracts-retrieval-response']['coredata']['prism:publicationName'];
+       		var tempId=temp['abstracts-retrieval-response']['coredata']['dc:identifier'].split(":");
+       		Obj.identifier= tempId[1];
+     	  	Obj.date = temp['abstracts-retrieval-response']['coredata']['prism:coverDate'];
+       		Obj.volume = temp['abstracts-retrieval-response']['coredata']['prim:volume'];
+       		Obj.affiliation= temp['abstracts-retrieval-response']['affilname'];
+       		Obj.author=temp['abstracts-retrieval-response']['authors'];     		
+		referenceObject.push(Obj);
+   	   }
+   	catch(e){
+       		console.log("JSON error");
+       		Obj.available=false;
+		referenceObject.push(Obj);
+    	}
+
+}
