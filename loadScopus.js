80ed3a108c8b53fcceebd5abbb04c305a13498a2
alvi0010 <alvintamie@gmail.com>
2012-09-09 09:10:11 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -24,7 +24,7 @@ function getContextCallback(response) {
   	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+context.scDocId+"?view=REF&startref=0");
 	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE");
 	var urlCoauthor=encodeURI( "http://api.elsevier.com/content/search/index:author?query=affil(university)&co-author="+context.au1Id);
-	var url=encodeURI("http://api.elsevier.com/content/search/index:scopus?query=author-name(ayam)");
+	var url=encodeURI("http://api.elsevier.com/content/search/index:scopus?query=ayam");
  //	gadgets.sciverse.makeContentApiRequest(urlCitedby, getCitedby, requestHeaders);
  //	gadgets.sciverse.makeContentApiRequest(urlCoauthor, getCoauthor, requestHeaders);
 		
