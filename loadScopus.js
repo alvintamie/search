bf6d00d05192e8318cf659a38ab6954f9960334b
alvi0010 <alvintamie@gmail.com>
2012-09-14 13:27:00 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -27,7 +27,7 @@ function getContextCallback(response) {
   	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+context.scDocId+"?view=REF&startref=0");
 	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE");
 	var urlCoauthor=encodeURI( "http://api.elsevier.com/content/search/index:author?query=affil(university)&co-author="+context.au1Id);
-	var url=encodeURI( "http://api.elsevier.com/content/search/index:scopus?query=all(\"Data driven smooth test for paired populations\")&sort=coverDate&facets=country(count=200,sort=fd);subjarea(sort=fd);pubyear(count=20,sort=na);authname(count=20,sort=fd);");
+	var url=encodeURI( "http://api.elsevier.com/content/search/index:scopus?query=all(\"Cytogenetic characterisation of the razor shells Ensis directus \")&sort=coverDate&facets=country(count=200,sort=fd);subjarea(sort=fd);pubyear(count=20,sort=na);authname(count=20,sort=fd);");
  //	gadgets.sciverse.makeContentApiRequest(urlCitedby, getCitedby, requestHeaders);
  //	gadgets.sciverse.makeContentApiRequest(urlCoauthor, getCoauthor, requestHeaders);
 
