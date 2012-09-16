f5431022db61f5a0673db17396e85f61e503bb73
alvi0010 <alvintamie@gmail.com>
2012-09-16 09:42:38 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -10,7 +10,6 @@ var requestHeaders = {};
  //var k        =60055420;
  var limitk   =60070000;
 
- 
 function getContextCallback(response) {
 	context = response;
 	
@@ -33,24 +32,31 @@ function getContextCallback(response) {
  //	setInterval(getNotRet,150);
  //	gadgets.sciverse.makeContentApiRequest(urlRelevantDocument, getR, requestHeaders);
  //	gadgets.sciverse.makeContentApiRequest(urlRef, getRef, requestHeaders);
- 	getCountry();
+ 	setInterval(getCountry(),150);
 }
-	var country="Angola";
+	var countryArray=new Array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos Islands", "Colombia", "Comoros", "Congo", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Czechoslovakia", "Democratic Republic Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Federated States of Micronesia", "Fiji", "Finland", "France", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macao", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Mariana Islands", "Norway", "Oman", "Pacific Islands", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Russian Federation", "Rwanda", "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea", "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United Nations", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City State", "Venezuela", "Viet Nam", "Virgin Islands", "Wallis and Futuna", "Western Sahara", "Yemen", "Yugoslavia", "Zaire", "Zambia", "Zimbabwe");
+	var statusWait=1;
+	var country;
+	var cc=0;
 function getCountry(){
-
-	for(var i=0;i<1;i++){
+	if(statusWait==0) return;
+	if(cc>3) return;
+	country=countryArray[cc];
 	var url=encodeURI( "http://api.elsevier.com/content/search/index:affiliation?query=affil("+country+")");
-	gadgets.sciverse.makeContentApiRequest(url, getInitialR, requestHeaders);
-	}
+	statusWait=0;
+	cc++;
+	gadgets.sciverse.makeContentApiRequest(url, getInitialR, requestHeaders);	
 }
 function getInitialR(response){
-	console.log("search-test");
+	console.log(country);
 	var temp = JSON.parse(parseValidator(response.text));	
 	var total=temp['search-results']['opensearch:totalResults'];
-	console.log("total = "+total);
+	console.log("total search = "+total);
+	if(total==0) { console.log("no result"); statusWait=1; return;}
 	for(var i=0;i<Math.ceil(total/200);i++){
 	var url=encodeURI( "http://api.elsevier.com/content/search/index:affiliation?query=affil("+country+")&start="+(200*i)+"&count=200");
-	gadgets.sciverse.makeContentApiRequest(url,getR,requestHeaders);	
+	if(i==Math.ceil(total/200)-1) gadgets.sciverse.makeContentApiRequest(url,getRLast,requestHeaders);
+	else			      gadgets.sciverse.makeContentApiRequest(url,getR,requestHeaders);
 	}
 }
 function getR(response){
@@ -63,6 +69,18 @@ function getR(response){
 		try {console.log(buffer[i]['country']);} catch(e){console.log("null");}
 		try {console.log(buffer[i]['dc:identifier']);} catch(e) {console.log("null");}
 	}
+}
+function getRLast(response){
+//	console.log("search-test");
+	var temp = JSON.parse(parseValidator(response.text));
+	var buffer=returnArray(temp['search-results']['entry']);
+	for(var i=0;i<buffer.length;i++){
+		try {console.log(buffer[i]['affiliation-name']);} catch(e) {console.log("null");}
+		try {console.log(buffer[i]['city']);} catch(e){console.log("null");}
+		try {console.log(buffer[i]['country']);} catch(e){console.log("null");}
+		try {console.log(buffer[i]['dc:identifier']);} catch(e) {console.log("null");}
+	}
+	statusWait=1;
 }
 function getTInit(){
 	if(k>limitk) return;
