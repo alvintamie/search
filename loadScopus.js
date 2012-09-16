193ec075a5f8f28645010068bb798ca72dc49163
alvi0010 <alvintamie@gmail.com>
2012-09-16 02:05:53 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -35,7 +35,7 @@ function getContextCallback(response) {
         requestHeaders['Accept'] = "application/json, text/xml";
   
   	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+context.scDocId+"?view=REF&startref=0");
-	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE&facets=country(count=200,sort=fd);");
+	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId);
 	var urlCoauthor=encodeURI( "http://api.elsevier.com/content/search/index:author?query=affil(university)&co-author="+context.au1Id+"&facets=country(count=200,sort=fd);");
 	var url=encodeURI( "http://api.elsevier.com/content/search/index:scopus?query=all(\"apple\")&sort=coverDate&facets=country(count=200,sort=fd);subjarea(count=100,sort=fd);pubyear(count=20);authname(count=20,sort=fd);");
  	gadgets.sciverse.makeContentApiRequest(urlCitedby, getCitedby, requestHeaders);
