5cf9f47d1e630fcbe7dae3691fa263dade5b1213
alvi0010 <alvintamie@gmail.com>
2012-09-16 00:26:51 -0700

    Update loadCitedby.js

--- loadCitedby.js
+++ loadCitedby.js
@@ -4,7 +4,8 @@ var totalCitation=-1;
 var totalLevelCitation=0;
 var currentLevelCitation=-1;
 var lastLevelCitation=0;
-// check every property
+var affiliationCitation= new Array();
+
 function upCitedby(){  
   if(currentLevelCitation==-1 || currentLevelCitation==totalLevelCitation) return false;
   if(currentLevelCitation==totalLevelCitation-1){
@@ -26,61 +27,25 @@ function downCitedby(){
 }
 
 function getCitedby(response){
-    	console.log("citedby initial")
-	var temp = JSON.parse(parseValidator(response.text));
+    	console.log("citedby initial");
+    	var temp = JSON.parse(parseValidator(response.text));
 	console.log(temp);
-	if(!statusCitedby){
-	try	{totalCitation= temp['search-results']['opensearch:totalResults'];}
-	catch(e){ updateCitedBy(); return;}
-		statusCitedby=true;}
-		
-	try{ if(temp['service-error']['status']['statusCode']=='INVALID_INPUT'){
-		console.log("No citedby");
-		return;}}
-	catch(e){
-	var buffer;
-	for(var i=0;i<temp['search-results']['entry'].length;i++){
-		var Obj= new Object();
-		if( Object.prototype.toString.call( temp ['search-results']['entry']) === '[object Array]' ) {
-		       buffer= temp['search-results']['entry'][i];}
-		else{  buffer= temp['search-results']['entry'];}
-		Obj.Abstract = buffer['dc:description'];
-		Obj.title =    buffer['dc:title'];
-       		Obj.type =     buffer['subtypeDescription'];
-       		Obj.citedbyCount = buffer['citedby-count'];
-       		Obj.creator= buffer['creator'];
-       		Obj.publicationName = buffer['prism:publicationName'];
-       		var tempId=buffer['dc:identifier'].split(":");
-       		Obj.identifier= tempId[1];
-     	  	Obj.date =buffer['prism:coverDate'];
-       		Obj.volume = buffer['prim:volume'];
-       		Obj.author=returnArray(buffer['author']);     	
-       		Obj.affiliation= returnArray(buffer['affiliation']);
-       		Obj.url="http://www.scopus.com/record/display.url?eid=2-s2.0-"+tempId[1]+"&origin=resultslist&sort=plf-f&src=s";
-       	
-		citedbyObject.push(Obj);
-		console.log(Obj);
-		}
-		if(totalCitation%25==0) { totalLevelCitation= Math.floor(totalCitation/25); lastLevelCitation=25;}
-		else 			{ totalLevelCitation= Math.floor(totalCitation/25)+1;lastLevelCitation=totalCitation%25;}
-		currentLevelCitation=1;
-		updateCitedBy();
-	}
-	
-	
-}
-function returnArray(a){
-		if( Object.prototype.toString.call( a ) === '[object Array]' ) {
-		       return a;}
-		else{  var b = new Array();
-		       b.push(a);
-		       return b;}
+	putCitedbyData(response);
+	affiliationCitation=returnArray(temp['search-results']['facet']['category']);
+	if(totalCitation%25==0) { totalLevelCitation= Math.floor(totalCitation/25); lastLevelCitation=25;}
+	else 			{ totalLevelCitation= Math.floor(totalCitation/25)+1;lastLevelCitation=totalCitation%25;}
+	currentLevelCitation=1;
+	updateCitedBy();
 }
 
 function getMoreCitedby(response){
-    	console.log("citedby initial")
-	var temp = JSON.parse(parseValidator(response.text));
+    	console.log("citedby more initial");
+    	var temp = JSON.parse(parseValidator(response.text));
 	console.log(temp);
+    	putCitedbyData(response);
+	updateCitedBy();}
+
+function putCitedbyData(response){
 	if(!statusCitedby){
 	try	{totalCitation= temp['search-results']['opensearch:totalResults'];}
 	catch(e){ updateCitedBy(); return;}
@@ -109,14 +74,17 @@ function getMoreCitedby(response){
        		Obj.author=returnArray(buffer['author']);     	
        		Obj.affiliation= returnArray(buffer['affiliation']);
        		Obj.url="http://www.scopus.com/record/display.url?eid=2-s2.0-"+tempId[1]+"&origin=resultslist&sort=plf-f&src=s";
-       	
 		citedbyObject.push(Obj);
 		console.log("more citedby");
 		console.log(Obj);
 		}
-	updateCitedBy();
-	
 	}
-	
-	
+}
+
+function returnArray(a){
+		if( Object.prototype.toString.call( a ) === '[object Array]' ) {
+		       return a;}
+		else{  var b = new Array();
+		       b.push(a);
+		       return b;}
 }
