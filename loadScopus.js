62a2ffc481fa59493aa4cf7259d6ca21ff228289
alvi0010 <alvintamie@gmail.com>
2012-09-17 01:52:38 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -63,7 +63,7 @@ function getInitialR(response){
 	if(total==0) { console.log("no result"); statusWait=1; return;}
 	justCount=0; justLast=Math.ceil(total/200);
 	for(var i=0;i<Math.ceil(total/200);i++){
-		waiting(200);
+	//	waiting(200);
 	var url=encodeURI( "http://api.elsevier.com/content/search/index:affiliation?query=affil("+country+")&start="+(200*i)+"&count=200");
 //	gadgets.sciverse.makeContentApiRequest(url,getR,requestHeaders);
 	}
