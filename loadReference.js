6adea14bada895842eadd5327384d57569a12247
alvi0010 <alvintamie@gmail.com>
2012-09-17 08:58:39 -0700

    Update loadReference.js

--- loadReference.js
+++ loadReference.js
@@ -57,7 +57,7 @@ function referenceQuery(buffer){
 	}
 	urlReference=encodeURI(urlReference+")&view=COMPLETE&facets=country(count=200,sort=fd);");
 	scopusId=buffer[0]['scopus-id'];
-	urlReference=encodeURI("http://api.elsevier.com/content/search/index:SCOPUS?query=ARTNUM("+scopusId+")");
+	urlReference=encodeURI("http://api.elsevier.com/content/search/index:SCOPUS?query=REFEID("+scopusId+")");
 	gadgets.sciverse.makeContentApiRequest(urlReference, getReference, requestHeaders);
 }
 
