350f97a87ffd167edc950e25c6a2da518096c556
alvi0010 <alvintamie@gmail.com>
2012-09-18 18:44:17 +0800

    Update loadReference.js

--- loadReference.js
+++ loadReference.js
@@ -2,7 +2,6 @@ var idToIndex= new Object();
 var readyRef=0;
 var numberRef=0;
 var currentReferenceSize=0;
-var affiliationReference= new Array();
 var referenceObject = new Array();
 var referenceObjectTemp;
 var referenceSize;
