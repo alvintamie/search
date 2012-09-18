337242a794eee3d3c04d6935fcf18220b17d3caa
alvi0010 <alvintamie@gmail.com>
2012-09-18 10:21:54 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -1,4 +1,5 @@
 var requestHeaders = {};
+var authorObject=new Object();
 var scopusId;
 var it;
 var k=4937;
@@ -13,16 +14,23 @@ function getContextCallback(response) {
         requestHeaders['X-ELS-APIKey'] = "0efd37da9830a0e7f43dbe261f5f7956";
         requestHeaders['X-ELS-Authtoken'] = context.secureAuthtoken;      
         requestHeaders['Accept'] = "application/json, text/xml";
-  
+  	initializeAuthorObject(response);
+  	var urlAuthor=encodeURI("http://api.elsevier.com/content/author/author_id:"+context.au1Id);
   	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+context.scDocId+"?view=REF");
 	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE&facets=country(count=200,sort=fd);");
 	var urlCoauthors=encodeURI( "http://api.elsevier.com/content/search/index:author?query=affil(university)&co-author="+context.au1Id+"&count=200&facets=country(count=200,sort=fd);");
         //gadgets.sciverse.makeContentApiRequest(urlCitedby, getCitedby, requestHeaders);
  	//gadgets.sciverse.makeContentApiRequest(urlCoauthors, getCoauthors, requestHeaders);
+ 	gadgets.sciverse.makeContentApiRequest(urlAuthor, getAuthor, requestHeaders);
  	gadgets.sciverse.makeContentApiRequest(urlRef, getRef, requestHeaders);
  	//searchEngineTesting();
 }
 
+function getAuthor(response){
+	console.log("main author");
+	var temp = JSON.parse(response.data);
+	console.log(temp);
+}
 
 function waiting( ms )
 {
@@ -36,4 +44,8 @@ function parseValidator(b){
 		while(b.indexOf("\"$\" :\}")>0){
     			b=b.replace("\"$\" :\}","    }");	}
     		return b;
+}
+
+function initializeAuthorObject(response){
+	
 }
