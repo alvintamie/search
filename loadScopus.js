dcede4dbc30470ec90dfd1d02ae421c5da68ffc2
alvi0010 <alvintamie@gmail.com>
2012-09-18 11:02:16 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -14,7 +14,6 @@ function getContextCallback(response) {
         requestHeaders['X-ELS-APIKey'] = "0efd37da9830a0e7f43dbe261f5f7956";
         requestHeaders['X-ELS-Authtoken'] = context.secureAuthtoken;      
         requestHeaders['Accept'] = "application/json, text/xml";
-  	initializeAuthorObject(response);
   	var urlAuthor = encodeURI("http://api.elsevier.com/content/search/index:author?query=auid("+context.au1Id+")");
 	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+context.scDocId+"?view=REF");
 	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE&facets=country(count=200,sort=fd);");
@@ -30,6 +29,13 @@ function getAuthor(response){
 	console.log("main author");
 	var temp = JSON.parse(response.data);
 	console.log(temp);
+	var buffer = returnArray(temp['search-results']['entry'])[0]['affiliation-current'];
+	authorObject.country=buffer['affiliation-country'];
+	authorObject.city=buffer['affiliation-city'];
+	authorObject.id=buffer['affiliation-id'];
+	authorObject.affiliationName=buffer['affiliation-name'];
+	authorObject.name=context.au1;
+	authorObject.title=context.docTitle;
 	
 }
 
