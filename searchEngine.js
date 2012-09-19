f3029992d621c89d442ca9d99de45b734939815e
alvi0010 <alvintamie@gmail.com>
2012-09-19 10:41:57 -0700

    Update searchEngine.js

--- searchEngine.js
+++ searchEngine.js
@@ -151,7 +151,7 @@ query=query+"&count=100";
 query=query+"&sort="+querySort;
 query=query+"&view=COMPLETE&facets=country(count=200,sort=fd);subjarea(count=100,sort=fd);pubyear(count=20,sort=na);authname(count=20,sort=fd);";
 console.log(query);
-console.log("queryList "+queryList[0].string[0].value)
+//console.log("queryList "+queryList[0].string[0].value)
 gadgets.sciverse.makeContentApiRequest(encodeURI(query), getSearchRequest, requestHeaders);
 }
 
