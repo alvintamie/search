9b159c8112ddc1fc32e6f23b28e2a0dfb863c419
alvi0010 <alvintamie@gmail.com>
2012-09-22 00:48:07 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -29,7 +29,7 @@ function startingRequest(response){
 	authorObject.title=context.docTitle;	
 
 	var urlRef = encodeURI("http://api.elsevier.com/content/abstract/scopus_id:"+context.scDocId+"?view=REF");
-	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE&facets=country(count=200,sort=fd);");
+	var urlCitedby = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=refeid(2-s2.0-"+context.scDocId+")&view=COMPLETE&start=0&count=200&facets=country(count=200,sort=fd);");
 	var urlCoauthors=encodeURI( "http://api.elsevier.com/content/search/index:author?query=affil(university)&co-author="+context.au1Id+"&count=200&facets=country(count=200,sort=fd);");
         urlTest = encodeURI("http://api.elsevier.com/content/search/index:scopus?query=all(quantum) AND AFFIL((singapore) OR (japan))&facets=country(count=200,sort=fd);");
  	gadgets.sciverse.makeContentApiRequest(urlCitedby, getCitedby, requestHeaders);
