2ede5ca0bbae48be4e8e75dd2af1d15d979f00ef
alvi0010 <alvintamie@gmail.com>
2012-09-23 15:45:01 -0700

    Update loadScopus.js

--- loadScopus.js
+++ loadScopus.js
@@ -106,7 +106,7 @@ function startingRequestAgain(response){
 		
 		var urlAuthor = encodeURI("http://api.elsevier.com/content/search/index:author?query=auid("+context.authorId+")");
   
-	  	authorObject.name=Obj.creator" halo ";
+	  	authorObject.name=Obj.creator+" halo ";
 		authorObject.title=Obj.title;	
 		var div= document.getElementById('judul');
 	       	div.innerHTML= "<b>"+context.docTitle+"</b><br>"+context.au1;
